101b3039394b3886c9cbc841afa4c35b34af9ee3
Android Studio <devandroid@ort>
2026-08-05 08:37:22 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP05_Martinez_Loufer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
