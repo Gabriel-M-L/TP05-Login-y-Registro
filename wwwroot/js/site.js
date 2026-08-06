82196ee8bde67c32de4afefa46bab1db3a3e126c
Android Studio <devandroid@ort>
2026-08-06 09:03:01 -0300

    falta menos js y falta bd

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿
+﻿function VerificarSesion() {
+    var nombreUsuario = document.getElementById("nombreUsuario").value;
+    var password = document.getElementById("password").value;
+    var error = document.getElementById("error");
+    var cuentaError = 0;
+
+    if (nombreUsuario.length < 8) {
+        error.innerHTML = "El nombre de usuario debe tener al menos 8 caracteres.";
+        error.style.color = "red";
+        cuentaError++;
+    }   
+    if (password.length < 8) {
+        error.innerHTML = "La contraseña debe tener al menos 8 caracteres.";
+        error.style.color = "red";
+        cuentaError++;
+    }
+    if (cuentaError > 0) {
+        return;
+    }
+}
